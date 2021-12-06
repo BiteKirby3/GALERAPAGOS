3ad9c74be9bab3942d26dd6b9628c03c2b77b630
romaneguari <romane.guari@etu.utc.fr>
2021-12-06 12:50:36 +0100

    add

--- backend/Agents/agents.go
+++ backend/Agents/agents.go
@@ -1,1 +1,56 @@
 package Agents
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Joueur struct {
+	ID        string
+	egoisme   int // de 0 à 10 si 10 : joueur très égoiste!
+	pecheur   bool
+	bricoleur bool
+	prefs     []Joueur
+}
+
+func MakePrefs(j Joueur, autres []Joueur) (prefs []Joueur) {
+	//TODO : selon le caractère de j et le caractère des autres joueurs faire une liste de pref
+	return autres
+}
+func NewJoeurEgoiste(id string) Joueur {
+	var prefs []Joueur
+	return Joueur{id, 10, false, false, prefs}
+}
+
+func NewJoueurAltruiste(id string) Joueur {
+	var prefs []Joueur
+	return Joueur{id, 0, false, false, prefs}
+}
+
+func CheckJoueurs(joueurs []Joueur) error {
+	if len(joueurs) < 3 || len(joueurs) > 12 {
+		err := fmt.Sprint("Le nombre de joueurs doit être compris entre 3 et 12.")
+		return errors.New(err)
+	}
+	for _, j := range joueurs {
+		if j.pecheur && j.bricoleur {
+			err := fmt.Sprint("Le joueur ", j.ID, " ne peut pas être pêcheur ET bricoleur.")
+			return errors.New(err)
+		}
+		if j.egoisme > 10 || j.egoisme < 0 {
+			err := fmt.Sprint("Le niveau d'égoïsme du joueur ", j.ID, " doit être compris entre 0 et 10.")
+			return errors.New(err)
+		}
+	}
+	return nil
+}
+
+func DevientPecheur(j Joueur) Joueur {
+	j.pecheur = true
+	return j
+}
+
+func DevientBricoleur(j Joueur) Joueur {
+	j.bricoleur = true
+	return j
+}
---
+++ backend/cmd/main.go
@@ -0,0 +1,8 @@
+package cmd
+
+import "gitlab.utc.fr/ia04_group/galerapagos_ia04/backend/Agents"
+
+func main() {
+	var j1, j2, j3 Agents.Joueur
+	j1 = Agents.NewJoueur
+}
