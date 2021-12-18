97a7d670c11dae214a976e8345607952d6a1ddca
Hiba <hiba.hammi@etu.utc.fr>
2021-12-18 18:34:16 +0100

    update playersForm.js and added gameForm.js and manage flow

--- frontend/pages/components/homeForm.js
+++ frontend/pages/components/homeForm.js
@@ -3,9 +3,9 @@ import React from "react";
 class HomeForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {      
-        nbPlayers: 2,
-        nbTurns : 2,   
+      this.state = {
+        nbPlayers: 3,
+        nbTurns : 2,         
       };
       this.handleChangeNbPlayers = this.handleChangeNbPlayers.bind(this);
       this.handleChangeNbTurns = this.handleChangeNbTurns.bind(this);
@@ -15,8 +15,8 @@ class HomeForm extends React.Component {
     handleChangeNbPlayers(event) {    this.setState({nbPlayers: event.target.value});  }
     handleChangeNbTurns(event) {    this.setState({nbTurns: event.target.value});  }
     handleSubmit(event) {
-      alert('Nombre de joueurs : ' + this.state.nbPlayers+"\n"+'Nombre de tours : ' + this.state.nbTurns);
       event.preventDefault();
+      this.props.onSubmit(this.state.nbPlayers,this.state.nbTurns)
     }
   
     render() {
@@ -24,12 +24,12 @@ class HomeForm extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <p>
           <label>
-            Nombre de joueurs : <input type="number" value={this.state.nbPlayers} onChange={this.handleChangeNbPlayers} />     
+            Nombre de joueurs : <input type="number" step={1} min={3} max={12} value={this.state.nbPlayers} onChange={this.handleChangeNbPlayers} />     
           </label>
           </p>
           <p>
           <label>
-            Nombre de tours : <input type="number" value={this.state.nbTurns} onChange={this.handleChangeNbTurns} />    
+            Nombre de tours : <input type="number" step={1} min={2} max={20} value={this.state.nbTurns} onChange={this.handleChangeNbTurns} />    
           </label>
           </p>
           <input type="submit" value="Suivant" />
--- frontend/pages/components/playersForm.js
+++ frontend/pages/components/playersForm.js
@@ -1,65 +1,97 @@
 import React from "react";
 import styles from '../../styles/Players.module.css';
+
+const ROLE_FISHERMAN = "fisherman"
+const ROLE_HANDYMAN = "handyman"
+const ROLE_NONE = "none"
+
 class PlayersForm extends React.Component {
     constructor(props) {
         super(props);
+        const players = []
+        for (var i = 0; i < this.props.nbPlayers; i++) {
+            players.push({
+                id : i,
+                name : "",
+                role : ROLE_NONE,
+                selfishness : 0, 
+                intelligence : 0,   
+            });
+        }
         this.state = {
-            name : "",
-            selfishness : 0, 
-            intelligence : 0,     
+            players  
         };
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangeSelfishness = this.handleChangeSelfishness.bind(this);
-    this.handleChangeIntelligence = this.handleChangeIntelligence.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeSelfishness = this.handleChangeSelfishness.bind(this);
+        this.handleChangeIntelligence = this.handleChangeIntelligence.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
     handleSubmit(event) {
         event.preventDefault();
+        this.props.onSubmit(this.state.players);
       }
 
-    handleChangeName(event){
-        this.setState({Name: event.target.value});
+    updatePlayer(index, attributes){
+        let players = [...this.state.players];
+        let player = {
+            ...players[index],
+            ...attributes
+        }
+        players[index] = player;
+        this.setState({players});
     }
-    handleChangeSelfishness(event){
-        this.setState({selfishness: event.target.value});
+
+    handleChangeName(event, index){
+        this.updatePlayer(index,{name: event.target.value})
     }
-    handleChangeIntelligence(event){
-        this.setState({intelligence: event.target.value});
+    handleChangeRole(event, index){
+        this.updatePlayer(index,{role: event.target.value})
     }
+    handleChangeSelfishness(event, index){
+        this.updatePlayer(index,{selfishness: Number(event.target.value)})
+    }
+    handleChangeIntelligence(event, index){
+        this.updatePlayer(index,{intelligence:  Number(event.target.value)})
+    }
+    
   
     render() {
       return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
             <div className={styles.scrollmenu}>
-                <div>
-                    Players 1 :
-                    <p>
-                    <label>
-                        Nom : <input type="text" value={this.state.name} onChange={this.handleChangeName} />     
-                    </label>
-                    </p>
-                    <p>
-                    <label>
-                        Rôle : <select>
-                            <option selected value="none">Rien</option>
-                            <option value="fisherman">Pêcheur</option>
-                            <option value="handyman">Bricoloeur</option>
-                        </select>    
-                    </label>
-                    </p>
-                    <p>
+                {Array.from({length: this.props.nbPlayers},(_, i) => (
+                    <div key={i}>
+                        Players {i+1} :
+                        <p>
                         <label>
-                            Egoïsme : <input type="number" value={this.state.selfishness}  min={0} max={10} step={1} onChange={this.handleChangeSelfishness}/>   
+                            Nom : <input type="text" value={this.state.players[i].name} onChange={event => this.handleChangeName(event, i)} />     
                         </label>
-                    </p>
-                    <p>
+                        </p>
+                        <p>
                         <label>
-                            Intelligence : <input type="number" value={this.state.intelligence}  min={0} max={10} step={1} onChange={this.handleChangeIntelligence}/>   
+                            Rôle : <select value={this.state.players[i].role} onChange={event => this.handleChangeRole(event, i)}>
+                                <option defaultValue={ROLE_NONE}>Rien</option>
+                                <option value={ROLE_FISHERMAN}>Pêcheur</option>
+                                <option value={ROLE_HANDYMAN}>Bricoloeur</option>
+                            </select>    
                         </label>
-                    </p>
+                        </p>
+                        <p>
+                            <label>
+                                Egoïsme : <input type="number" value={this.state.players[i].selfishness}  min={0} max={10} step={1} onChange={event => this.handleChangeSelfishness(event, i)}/>   
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Intelligence : <input type="number" value={this.state.players[i].intelligence}  min={0} max={10} step={1} onChange={event => this.handleChangeIntelligence(event, i)}/>   
+                            </label>
+                        </p>
+
                 </div>
+                ))     
+                }
             </div>
           <input type="submit" value="Suivant" />
         </form>
